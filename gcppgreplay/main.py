033a5a851d04e957a7452c0569b355e9d154685d
Shubham Patil <patil.sm17@gmail.com>
2020-07-09 07:10:34 +0530

    0.0.1 #commen change parse algorithm to support log merging

--- gcppgreplay/main.py
+++ gcppgreplay/main.py
@@ -13,11 +13,12 @@ from datetime import datetime, timedelta
 warnings.filterwarnings("ignore")
 
 log_splitter = ":"
+payload = "textPayload"
 logging_df = "%Y-%m-%dT%H:%M:%S.%fZ"
-
-blacklist_sql = [
-    "Connection reset by peer"
-]
+log_part_pattern = r"^(\((?P<part>\d+)/(?P<parts>\d+)\)\s)"
+log_identity_pattern = log_part_pattern + r"?\[\d+\]:\s\[\d+-\d+]\sdb=({database}),user=({users})"
+req_init_pattern = log_identity_pattern + r"\s(LOG|DETAIL)"
+log_init_pattern = log_identity_pattern.format(database=r"\S+", users=r"\S+")
 
 
 class DatetimeParseAction(argparse.Action):
@@ -43,61 +44,94 @@ def reset_verbose_mode(args):
         log.basicConfig(format=log_format)
 
 
-def print_log(data, sink):
-    if data["group"].endswith("LOG") and data["sql"] not in blacklist_sql:
-        data["sql"] = data["sql"] + ";" if data["sql"][-1] != ";" else data["sql"]
-        print(data["sql"], file=sink)
-
-
 def print_formatted_current(current_log, next_log, sink):
-    parsed_args = {}
+    if current_log is None or next_log is None:
+        return
 
-    if next_log["action"] == "parameters":
-        temp_sql = next_log["sql"]
-        key_args = [int(x.strip('$')) for x in re.findall("\$\d+", current_log["sql"])]
-        for idx, key in enumerate(key_args):
-            current_arg = "${pos} = ".format(pos=key)
-            next_arg = "${pos} = ".format(pos=(key + 1))
-            start = "^(?P<start>\\{current_arg})".format(current_arg=current_arg)
-            end = ("$" if idx == len(key_args) - 1 else "(?P<end>, \\{next_arg})".format(next_arg=next_arg))
-            pattern = "{start}(?P<value>.*){end}".format(start=start, end=end)
-            match = re.match(pattern, temp_sql)
-            if match:
-                temp_sql = next_arg + re.split(pattern, temp_sql, 1)[-1]
-                parsed_args["$" + str(key)] = match.group("value")
+    if current_log["group"].endswith("LOG"):
+        if next_log["group"].endswith("DETAIL") and next_log["action"] == "parameters":
 
-        for key in sorted(key_args, reverse=True):
-            arg = "$" + str(key)
-            current_log["sql"] = current_log["sql"].replace(arg, parsed_args[arg])
+            parsed_args = {}
+            temp_sql = next_log["sql"]
+            key_args = [int(x.strip('$')) for x in re.findall("\$\d+", current_log["sql"])]
+            for idx, key in enumerate(key_args):
+                current_arg = "${pos} = ".format(pos=key)
+                next_arg = "${pos} = ".format(pos=(key + 1))
+                start = "^(?P<start>\\{current_arg})".format(current_arg=current_arg)
+                end = ("$" if idx == len(key_args) - 1 else "(?P<end>, \\{next_arg})".format(next_arg=next_arg))
+                pattern = "{start}(?P<value>.*){end}".format(start=start, end=end)
+                match = re.match(pattern, temp_sql)
+                if match:
+                    temp_sql = next_arg + re.split(pattern, temp_sql, 1)[-1]
+                    parsed_args["$" + str(key)] = match.group("value")
 
-    print_log(current_log, sink)
+            for key in sorted(key_args, reverse=True):
+                arg = "$" + str(key)
+                current_log["sql"] = current_log["sql"].replace(arg, parsed_args[arg])
+
+        current_log["sql"] = current_log["sql"] + ";" if current_log["sql"][-1] != ";" else current_log["sql"]
+        print(current_log["sql"], file=sink)
 
 
 def generate_query_filter(args):
     filters = 'resource.type="cloudsql_database" AND ' \
               'timestamp >= "{start_time}" AND ' \
               'timestamp <= "{end_time}" AND ' \
-              '("LOG:" OR "DETAIL:")'
+              'resource.labels.database_id="{project}:{instance}"'
 
-    filters += ' AND resource.labels.database_id="{project}:{host}"' if args["host"] is not None else ''
-    filters += ' AND ({})'.format(" OR ".join(['"db={database},user={user}"'
-                                              .format(database=args["database"], user=user)
-                                               for user in set(args["user"])]))
     filters += ' AND ({custom_filters})' if args["custom_filters"] is not None else ''
     return filters.format(**args)
+
+
+def merge_payloads(first_payload, next_payload, merge_index):
+    first_match = re.match(log_part_pattern, first_payload)
+    next_match = re.match(log_part_pattern, next_payload)
+
+    if first_match is None and next_match is None:
+        first_payload += next_payload
+
+    elif first_match is not None and next_match is not None and \
+            int(first_match.group("part")) == 1 and int(next_match.group("part")) == (merge_index + 1):
+        first_payload += re.split(log_part_pattern, next_payload, maxsplit=1)[-1]
+
+    else:
+        raise Exception("Merger out of sync !")
+
+    return first_payload
+
+
+def get_data_node(req_log):
+    splits = req_log[payload].split(log_splitter) if req_log is not None else None
+    if splits is not None and len(splits) >= 4:
+        return {
+            "pid": splits[0].strip(),
+            "group": splits[1].strip(),
+            "action": splits[2].strip(),
+            "sql": log_splitter.join(splits[3:]).strip()
+        }
+
+    else:
+        return None
 
 
 def generate_logs(args):
     credentials, project = google.auth.default()
     logging_client = logging.Client(credentials=credentials)
+    filters = generate_query_filter(args)
+    log.debug("gCloud Query Filters : '%s'", filters)
 
     with args["output"] as out:
-        current_log, next_log = None, None
-        filters = generate_query_filter(args)
-        log.debug("Using gcloud read filters : '%s'", filters)
+        args["users"] = r"\S+" if args["users"] is None else r"|".join([u for u in set(args["users"])])
+        query_init_pattern = req_init_pattern.format(**args)
+        log.debug("Log Init Pattern : %s", log_init_pattern)
+        log.debug("Query Init Pattern : %s", query_init_pattern)
 
+        prev_req_log, curr_req_log, curr_entry = None, None, None
         iterator = logging_client.list_entries(projects=[args["project"]], filter_=filters, page_size=args["page_size"])
         pages = iterator.pages
+
+        is_req_init = False
+        merge_index = 1
 
         while True:
             try:
@@ -106,34 +140,28 @@ def generate_logs(args):
                 break
 
             for entry in page:
-                payload = entry.to_api_repr()['textPayload']
-                splits = payload.split(log_splitter)
+                log.debug("Received : %s", entry)
+                curr_entry = entry.to_api_repr()
 
-                data = {
-                    "pid": splits[0].strip(),
-                    "group": splits[1].strip(),
-                    "action": splits[2].strip(),
-                    "sql": log_splitter.join(splits[3:]).strip()
-                }
+                is_init = (re.match(log_init_pattern, curr_entry[payload]) is not None)
 
-                if next_log is None:
-                    if current_log is None:
-                        current_log = data
-                    else:
-                        next_log = data
-                        print_formatted_current(current_log, next_log, out)
+                if is_init:
+                    merge_index = 1
+                    is_req_init = (re.match(query_init_pattern, curr_entry[payload]) is not None)
+                    if is_req_init:
+                        print_formatted_current(get_data_node(prev_req_log), get_data_node(curr_req_log), out)
+                        prev_req_log = curr_req_log
+                        curr_req_log = curr_entry
 
                 else:
-                    current_log = next_log
-                    next_log = data
-                    print_formatted_current(current_log, next_log, out)
+                    if is_req_init:
+                        curr_req_log[payload] = merge_payloads(curr_req_log[payload], curr_entry[payload], merge_index)
+                        merge_index += 1
 
             time.sleep(args["delay"])
 
-        if current_log is not None and next_log is None:
-            print_log(current_log, out)
-        elif next_log is not None:
-            print_log(next_log, out)
+        print_formatted_current(get_data_node(prev_req_log), get_data_node(curr_req_log), out)
+        print_formatted_current(get_data_node(curr_req_log), get_data_node(curr_req_log), out)
 
 
 if __name__ == "__main__":
@@ -144,28 +172,28 @@ if __name__ == "__main__":
         epilog="Report bugs at patil.sm17@gmail.com")
 
     mandatory_group = parser.add_argument_group("mandatory arguments", "Args that must be supplied")
-    mandatory_group.add_argument("-P", "--project", dest="project", help="Google Cloud Project Id", required=True)
+    mandatory_group.add_argument("-P", "--project", dest="project", help="GCP project id", required=True)
+    mandatory_group.add_argument("-i", "--instance", dest="instance", help="GCP Cloud SQL instance name",
+                                 required=True)
+    mandatory_group.add_argument("-d", "--database", dest="database", help="GCP Cloud SQL postgres database name")
 
-    parser.add_argument("-h", "--host", dest="host", help="Google Cloud SQL Hostname")
-    parser.add_argument("-d", "--database", dest="database", help="Google Cloud SQL postgres instance name")
-    parser.add_argument("-u", "--user", dest="user", action="append", help="Google Cloud SQL Postgres User")
+    parser.add_argument("-u", "--user", dest="users", action="append", help="GCP Cloud SQL Postgres User")
     parser.add_argument("-st", "--start-time", dest="start_time",
                         action=DatetimeParseAction,
                         default=(datetime.now() - timedelta(days=7)).strftime(logging_df),
-                        help="Start time in standard ISO format. Defaults to 7 days ago.")
+                        help="Start time in standard UTC format. Defaults to 7 days ago.")
     parser.add_argument("-et", "--end-time", dest="end_time",
                         action=DatetimeParseAction, default=datetime.now().strftime(logging_df),
-                        help="End time in standard ISO format. Defaults to now.")
+                        help="End time in standard UTC format. Defaults to now.")
     parser.add_argument("-o", "--output", dest="output", type=argparse.FileType('w'), default=sys.stdout,
                         help="Output file to write in.")
-    parser.add_argument("-c", "--custom-filter", dest="custom_filters", help="Custom filters to add to the query")
-    parser.add_argument("-p", "--page-size", dest="page_size", type=int, default=10,
+    parser.add_argument("-c", "--custom-filters", dest="custom_filters", help="Custom filters to add to the query")
+    parser.add_argument("-p", "--page-size", dest="page_size", type=int, default=100,
                         help="Page size to fetch in a single API call (number of records)")
-    parser.add_argument("-w", "--wait", dest="delay", type=int, default=1,
+    parser.add_argument("-w", "--wait", dest="delay", type=int, default=2,
                         help="Delay between API calls (in seconds)")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode (shows debug logs)")
 
     args = vars(parser.parse_args())
-
     reset_verbose_mode(args)
     generate_logs(args)
